158a4465e5e7d120c90644939f55aa4a1989f3ea
Kavya VB <kavyavb620@gmail.com>
2023-04-27 11:53:57 +0530

    Add files via upload

---
+++ test_1pytest.py
@@ -0,0 +1,16 @@
+def test_add():
+    a=10
+    b=10
+    assert a==b
+
+def test_disp():
+    print("Working on pytest")
+
+def test_div():
+    assert 100//10
+
+
+
+
+
+
---
+++ test_2pytest.py
@@ -0,0 +1,5 @@
+def test_1():
+    print("Print something")
+
+def test_2():
+    print("Learning skills is important")
